ae7332005007b672495b1948aa71cacdc6f09358
Manuel Londoño <hbiaser132@gmail.com>
2022-11-21 16:15:18 +0000

    refactor: debounce file

--- src/Debounce.js
+++ src/Debounce.js
@@ -1,34 +1,27 @@
 /**
+ * @template T
  * @param {Object} props
- * @param {(value? : any[]) => Promise<void> | void} [props.fn]
  * @param {number} [props.delay]
  * @param {boolean} [props.immediate]
- * @returns
+ * @param {(value?: T) => void} [props.fn]
+ * @returns {(value: T) => void}
  */
-
 export function Debounce(props) {
   const {
-    fn = () => console.log("default function param of the Debounce"),
     delay = 300,
     immediate = false,
+    fn = () => console.log('default function param of the Debounce'),
   } = props;
 
-  let timerId;
+  let timerId = null;
 
-  return function executedFunction() {
-    let context = this;
-    let args = arguments;
-
-    let later = function () {
-      timerId = null;
-      if (!immediate) fn.apply(context, args);
-    };
-
-    let callNow = immediate && !timerId;
-
+  return function executedFunction(...args) {
     clearTimeout(timerId);
 
-    timerId = setTimeout(later, delay);
-    if (callNow) fn.apply(context, args);
+    if (immediate) return fn(...args);
+
+    timerId = setTimeout(() => {
+      fn(...args);
+    }, delay);
   };
 }
